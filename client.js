afb74b0f2625d331a27244506bfe26396bb3f731
Christian Bryan <christianbryan@gmail.com>
2011-02-26 17:54:15 -0500

    Oodles of fresh styles for the frontend, some Redis backed room tracking, and a handful of nice fade effects.

--- client.js
+++ client.js
@@ -1,33 +1,130 @@
+// Inject a console logger so as to not upset browsers that don't have this functionality.
 if (!window.console) { console = {}; console.log = function() {} }
+
+var playlist = new Array;
+var player;
+
+function onYouTubePlayerAPIReady() {
+    // player = new YT.Player('player', {
+    //     videoId: 'skKJNmW1czw',
+    //     playerVars: { 'controls': 0 },
+    //     events: { 'onStateChange': onStateChange }
+    // });
+}
+
+function onStateChange(state) {
+    console.log("Video state changed: " + state);
+    if (state == YT.PlayerState.ENDED) {
+        console.log("Video play ended, grabbing the next video.");
+        var new_video_id = playlist.pop;
+        updatePlaylist();
+        player.loadVideoById(new_video_id);
+    }
+}
+
+function updatePlaylist() {
+    $("#playlist").text(playlist.toString());
+}
 
 // The bulk of the client business gets handled in here.
 $(function() {
-    socket = new io.Socket(null, { port: 8002, rememberTransport: false });
-    socket.connect();
-
-    // Handle chat messages.
-    socket.on('message', function(data) {
-        // Build up the element and inject the text to take advantage
-        // of some HTML escaping goodness.
-        console.log(data);
-
-        var element = $("<li></li>");
-        element.text(data);
-        $("#message_list").append(element);
+    $('.default_value').each(function() {
+        var default_value = this.value;
+        $(this).focus(function() {
+            if(this.value == default_value) {
+                this.value = '';
+            }
+        });
+        $(this).blur(function() {
+            if(this.value == '') {
+                this.value = default_value;
+            }
+        });
     });
 
-    // Bind to the form submit and send the message to the server.
-    $("#chat").submit(function(event) {
+    // When the user picks a username, connect to the server.
+    $("#get_a_username").submit(function(event) {
         event.preventDefault();
-        var field = $("#message");
-        var message = field.val();
-        field.val("");
         
-        // if (socket.disconnected) {
-        //     socket.connect()
-        // }
+        var username = $("#username").val();
 
-        socket.send(message);
+        socket = new io.Socket(document.location.hostname, { port: document.location.port, rememberTransport: false });
+        socket.connect();
+
+        // Set the username on connect.
+        socket.on('connect', function() {
+            socket.send({ 'setUsername': username });
+            $("#username_select").fadeOut('fast');
+            $("#room_container").fadeIn('fast');
+        });
+
+        // Handle chat messages.
+        socket.on('message', function(data) {
+            // Build up the element and inject the text to take advantage
+            // of some HTML escaping goodness.
+            data = jQuery.parseJSON(data);
+            var element = $("<li></li>");
+            element.hide();
+
+            // Route to different actions based on message type.
+            if (data.hasOwnProperty("message")) {
+                element.append("<span class=\"username\">" + data["message"]["username"] + "</span>");
+                element.append(data["message"]["text"]);
+                $("#message_list").append(element);
+                $("#message_list").scrollTop(1000000);
+                element.fadeIn('fast');
+            } else if (data.hasOwnProperty("announcement")) {
+                element.addClass("announcement");
+                element.text(data["announcement"]["text"]);     
+                $("#message_list").append(element);
+                $("#message_list").scrollTop(1000000);
+                element.fadeIn('fast');
+            } else if (data.hasOwnProperty("videos")) {
+                console.log(data["videos"][0]);
+                playlist.unshift(data["videos"][0]);
+                updatePlaylist();
+            } else {
+                console.log("Caught an unroutable message!");
+                console.log(data);
+            }                                
+        });
+
+        // Bind to the form submit and send the message to the server.
+        $("#chat").submit(function(event) {
+            event.preventDefault();
+            var field = $("#message");
+            var message = field.val();
+            field.val("");
+
+            // Does the message text have a youtube link?
+            var regex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+            var match;
+            var video_ids = new Array;
+
+            while (match = regex.exec(message)) {
+                var url = match[0];
+                if (url.indexOf("youtube") > 0) {
+                    var youtube_id_matcher = /^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/;
+                    var id = youtube_id_matcher.exec(url);
+                    if (id != null) {
+                        video_ids.push(id[5]);
+                    }
+                }
+            }
+
+            if (socket.disconnected) {
+                socket.connect()
+            }
+            
+            // Send the video ids.
+            if (video_ids.length > 0) {
+                socket.send({ 'videos': video_ids });
+            }
+
+            socket.send({ 'message': message });
+            return false;
+        });
+
         return false;
     });
 });
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 PORT = 8002;
 HOST = '127.0.0.1';
 
@@ -5,13 +6,26 @@ var staticHandler = require('./staticHandler'),
     sys           = require('sys'),
     http          = require('http'),
     url           = require('url'),
-io            = require('./vendor/socket.io'),
-util          = require('util');
+    io            = require('./vendor/socket.io'),
+    util          = require('util'),
+    redis         = require('./vendor/node_redis');
+
+var redisSubscribe = redis.createClient(),
+    redisPublish   = redis.createClient(),  
+    redisStorage   = redis.createClient();
 
 send404 = function(response) {
     response.writeHead(404);
     response.write('404');
     response.end();
+}
+
+setUsername = function(sessionId, username) {
+    redisStorage.set(sessionId, username);
+}
+
+getUsername = function(sessionId) {
+    redisStorage.get(sessionId);
 }
 
 // Handle static files yeahhh.
@@ -22,6 +36,10 @@ var server = http.createServer(function(request, response) {
     switch(path) {
     case "/":
         var handler = staticHandler.serve('./index.html');
+        handler(request, response);
+        break;
+    case "/styles.css":
+        var handler = staticHandler.serve('./styles.css');
         handler(request, response);
         break;
     case "/client.js":
@@ -37,19 +55,49 @@ server.listen(PORT, HOST);
 
 // Let socket.io hook into the server.
 var socket = io.listen(server);
-socket.on('connection', function(client) {
-    socket.broadcast("welcome " + client.sessionId + "!");
 
+// Monitor the subscription connection and route messages to their clients.
+redisSubscribe.subscribe("chat");
+redisSubscribe.on("message", function(channel, message) {
+    var object = JSON.parse(message);
+    util.log("Got a message on channel: " + channel);
+    util.log(util.inspect(object));
+    socket.broadcast(message);
+});
+
+// Monitor the socket for connections.
+socket.on('connection', function(client) {
+    // Handle and route messages.
+    // Messages are always in the form of '{ commandType: payload }'
     client.on('message', function(data) {
-        socket.broadcast(data);
-        util.log('Recieved a message: ' + util.inspect(data));
+        if ('setUsername' in data) {
+            util.log(util.inspect(data.setUsername));
+            redisStorage.set(client.sessionId, data.setUsername, function(err, res) {
+                redisPublish.publish("chat",
+                                     JSON.stringify({ "announcement": { "text": data["setUsername"] + " has joined." }})
+                );
+            });
+        }
+        else if ('message' in data) {
+            redisStorage.get(client.sessionId, function(things) { util.log(util.inspect(things)); });
+
+            redisStorage.get(client.sessionId, function(err, res) {
+                redisPublish.publish("chat",
+                                     JSON.stringify({ "message": { "username": res, "text": data["message"] }})
+                );
+            });
+        }
+        else if ('videos' in data) {
+            redisPublish.publish("chat", JSON.stringify(data));
+        }
+        else {
+            util.log("No route found for incoming data!");
+        }
     });
 
     client.on('disconnect', function(data) {
-        util.log("Client disconnected.");
-        socket.broadcast("see ya later " + client.sessionId + "!");
+        redisPublish.publish("chat", JSON.stringify({ "announcement": { "text": getUsername(client.sessionId) + " has left." }}));
     });
-
 });
 
 util.log("Server at http://" + HOST + ':' + PORT.toString() + '/');
